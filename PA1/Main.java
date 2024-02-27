30bc9a49586e11e4013cf375e7b5dc909db6f901
Johna <johnpeterkrause@gmail.com>
2024-02-27 18:02:42 +0200

    first revision

--- PA1/Main.java
+++ PA1/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
 
+        RecursiveArray array1 = new RecursiveArray();
+        RecursiveArray array2 = new RecursiveArray("1,2,3,4,5,6,7,8,9,10");
+        RecursiveArray array3 = new RecursiveArray("1,1,3,4,9,6,2,8,7,13");
+
+        // System.out.println("Array 1: " + array1.array);
+
+        for (int i=0; i<array2.array.length; i++) {
+            System.out.print(array2.array[i]);
+        }
+        System.out.println();
+
+        System.out.println("Array 2: " + array2.toString());
+
+        array1.append(6);
+        System.out.println("Array 1: " + array1.toString());
+        array1.prepend(10);
+        System.out.println("Array 1: " + array1.toString());
+
+        System.out.println("Array 1 contains 10: " + array1.contains(10));
+        System.out.println("Array 1 contains 2: " + array1.contains(2));
+
+        array2.sortDescending();
+        System.out.println("Array 2: " + array2.toString());
+
+        array3.sortAscending();
+        System.out.println("Array 3: " + array3.toString());
+
+
     }
 }
--- PA1/RecursiveArray.java
+++ PA1/RecursiveArray.java
@@ -2,44 +2,170 @@ public class RecursiveArray {
     public Integer[] array;
 
     public RecursiveArray() {
-
+        array = new Integer[0];
     }
 
     public RecursiveArray(String input) {
+        if (input.isEmpty()) {
+            array = new Integer[0];
+        } else {
+            String[] split_ints = input.split(",");
+            array = new Integer[split_ints.length];
+            parseInts(split_ints, 0);
+        }
+    }
 
+    private void parseInts(String[] ints, int index) {
+        if (index < ints.length) {
+            array[index] = Integer.parseInt(ints[index]);
+            parseInts(ints, index + 1);
+        }
     }
 
     @Override
     public String toString() {
-        return "";
+        if (array.length == 0) {
+            return "Empty Array";
+        } else {
+            return "[" + parseChars(0) + "]";
+        }
+    }
+
+    private String parseChars(int index) {
+        if (index < array.length - 1) {
+            return array[index] + "," + parseChars(index + 1);
+        } else {
+            return String.valueOf(array[index]);
+        }
     }
 
     public void append(Integer value) {
+        Integer[] newArray = new Integer[array.length + 1];
+        appendHelper(newArray, value, 0);
+        array = newArray;
+    }
 
+    private void appendHelper(Integer[] newArray, Integer value, int index) {
+        if (index < array.length) {
+            newArray[index] = array[index];
+            appendHelper(newArray, value, index + 1);
+        } else {
+            newArray[index] = value;
+        }
     }
 
     public void prepend(Integer value) {
+        Integer[] newArray = new Integer[array.length + 1];
+        prependHelper(newArray, value, 0);
+        array = newArray;
+    }
 
+    private void prependHelper(Integer[] newArray, Integer value, int index) {
+        if (index < array.length) {
+            newArray[index + 1] = array[index];
+            prependHelper(newArray, value, index + 1);
+        } else {
+            newArray[0] = value;
+        }
     }
 
     public boolean contains(Integer value) {
-        return false;
+        return containsHelper(value, 0);
+    }
+
+    private boolean containsHelper(Integer value, int index) {
+        if (index < array.length) {
+            if (array[index] == value) {
+                return true;
+            } else {
+                return containsHelper(value, index + 1);
+            }
+        } else {
+            return false;
+        }
     }
 
     public boolean isAscending() {
-        return false;
+        return isAscendingHelper(0);
+    }
+
+    private boolean isAscendingHelper(int index) {
+        if (index < array.length - 1) {
+            if (array[index] > array[index + 1]) {
+                return false;
+            } else {
+                return isAscendingHelper(index + 1);
+            }
+        } else {
+            return true;
+        }
     }
 
     public boolean isDescending() {
-        return false;
+        return isDescendingHelper(0);
+    }
+
+    private boolean isDescendingHelper(int index) {
+        if (index < array.length - 1) {
+            if (array[index] < array[index + 1]) {
+                return false;
+            } else {
+                return isDescendingHelper(index + 1);
+            }
+        } else {
+            return true;
+        }
     }
 
     public void sortAscending() {
+        sortAscendingHelper(0);
+    }
 
+    private void sortAscendingHelper(int index) {
+        if (index < array.length - 1) {
+            int minIndex = findMinIndex(index, index + 1);
+            if (minIndex != index) {
+                swap(index, minIndex);
+            }
+            sortAscendingHelper(index + 1);
+        }
+    }
+
+    private int findMinIndex(int currentIndex, int minIndex) {
+        if (minIndex < array.length) {
+            if (array[minIndex] < array[currentIndex]) {
+                return findMinIndex(minIndex, minIndex + 1);
+            }
+        }
+        return currentIndex;
+    }
+
+    private void swap(int index1, int index2) {
+        int tempInt = array[index1];
+        array[index1] = array[index2];
+        array[index2] = tempInt;
     }
 
     public void sortDescending() {
-
+        sortDescendingHelper(0);
     }
 
+    private void sortDescendingHelper(int index) {
+        if (index < array.length - 1) {
+            int maxIndex = findMaxIndex(index, index + 1);
+            if (maxIndex != index) {
+                swap(index, maxIndex);
+            }
+            sortDescendingHelper(index + 1);
+        }
+    }
+
+    private int findMaxIndex(int currentIndex, int maxIndex) {
+        if (maxIndex < array.length) {
+            if (array[maxIndex] > array[currentIndex]) {
+                return findMaxIndex(maxIndex, maxIndex + 1);
+            }
+        }
+        return currentIndex;
+    }
 }
