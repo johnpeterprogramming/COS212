79e3dd629c7f558402b4bbb37237bb7bb5730d52
Johna <johnpeterkrause@gmail.com>
2024-02-27 13:15:19 +0200

    Initial project config

---
+++ PA1/RecursiveArray.java
@@ -0,0 +1,45 @@
+public class RecursiveArray {
+    public Integer[] array;
+
+    public RecursiveArray() {
+
+    }
+
+    public RecursiveArray(String input) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    public void append(Integer value) {
+
+    }
+
+    public void prepend(Integer value) {
+
+    }
+
+    public boolean contains(Integer value) {
+        return false;
+    }
+
+    public boolean isAscending() {
+        return false;
+    }
+
+    public boolean isDescending() {
+        return false;
+    }
+
+    public void sortAscending() {
+
+    }
+
+    public void sortDescending() {
+
+    }
+
+}
